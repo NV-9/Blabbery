5ceae0a4bf5f2f22e04d9f9c794399da045ab9d9
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-03-16 11:43:26 +0000

    Added url and api patterns

--- config/api.py
+++ config/api.py
@@ -1,12 +1,10 @@
 from django.conf import settings
-from rest_framework.routers import DefaultRouter
-from rest_framework.routers import SimpleRouter
-
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from blabbery.api import api_urlpatterns
 from blabbery.viewsets import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
-
 router.register("user", UserViewSet)
 
 app_name = "api"
-urlpatterns = router.urls
+urlpatterns = api_urlpatterns + router.urls
