a335c576d00b76352f6f5cba12d92b9a893de55a
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-03-16 11:45:33 +0000

    Added url patterns for main app

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings 
 from django.contrib import admin
 from django.urls import path, include
+from blabbery.urls import url_patterns as blabbery_url_patterns
 
 urlpatterns = [
     path('jet/', include('jet.urls', 'jet')),
     path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path(settings.ADMIN_URL, admin.site.urls),
     path(settings.API_URL, include('config.api')),
+    *blabbery_url_patterns,
 ]
