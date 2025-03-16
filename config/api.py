b7ebb036b0d21f53a17a9896b50262fae915f838
Viswamedha Nalabotu <nalabotuviswamedha@gmail.com>
2025-03-16 13:48:33 +0000

    Added models to api

--- config/api.py
+++ config/api.py
@@ -1,10 +1,15 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from blabbery.api import api_urlpatterns
-from blabbery.viewsets import UserViewSet
+from blabbery.viewsets import UserViewSet, BaseChatViewSet, DirectChatViewSet, GroupChatViewSet, PublicGroupChatViewSet, MessageViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 router.register("user", UserViewSet)
+router.register("base-chat", BaseChatViewSet)
+router.register("direct-chat", DirectChatViewSet)
+router.register("group-chat", GroupChatViewSet)
+router.register("public-group-chat", PublicGroupChatViewSet, basename="public-group-chat")
+router.register("message", MessageViewSet)
 
 app_name = "api"
 urlpatterns = api_urlpatterns + router.urls
